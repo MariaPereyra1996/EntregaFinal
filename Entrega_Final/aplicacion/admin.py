c69e178b97ec97f59f690277b3f0b02e38e416c2
MariaPereyra1996 <140986810+MariaPereyra1996@users.noreply.github.com>
2023-08-13 20:56:52 -0300

    Add files via upload

---
+++ Entrega_Final/aplicacion/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from.models import Suscripcion, Clientes, Servicios, Avatar
+
+admin.site.register(Suscripcion)
+admin.site.register(Clientes)
+admin.site.register(Servicios)
+

d6e2466b3b48ab1c2355c12d39e955a7562ed63b
MariaPereyra1996 <140986810+MariaPereyra1996@users.noreply.github.com>
2023-08-13 23:24:30 -0300

    Delete Entrega_Final directory

--- Entrega_Final/aplicacion/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from.models import Suscripcion, Clientes, Servicios, Avatar, Oficinas
-
-admin.site.register(Suscripcion)
-admin.site.register(Clientes)
-admin.site.register(Servicios)
-admin.site.register(Avatar)
-admin.site.register(Oficinas)
